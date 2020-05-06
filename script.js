2943f9b61a621a9e469a668b48ca1b688eb23f9c
Abhishek <karma1997.acharya@gmail.com>
2020-05-06 16:51:25 +0545

    sound alarm fixed

--- script.js
+++ script.js
@@ -8,23 +8,23 @@ function clock() {
   hours = fullDate.getHours();
   mins = fullDate.getMinutes();
   secs = fullDate.getSeconds();
-  day = hours > 12 ? "AM" : "PM";
+  day = hours > 12 ? 'AM' : 'PM';
 
   if (hours < 10) {
-    hours = "0" + hours;
+    hours = '0' + hours;
   }
   if (minutes < 10) {
-    mins = "0" + mins;
+    mins = '0' + mins;
   }
   if (seconds < 10) {
-    secs = "0" + secs;
+    secs = '0' + secs;
   }
-  document.getElementById("hour").innerHTML = hours;
-  document.getElementById("minutes").innerHTML = ":" + mins;
-  document.getElementById("seconds").innerHTML = ":" + secs;
+  document.getElementById('hour').innerHTML = hours;
+  document.getElementById('minutes').innerHTML = ':' + mins;
+  document.getElementById('seconds').innerHTML = ':' + secs;
 
-  day = hours < 12 ? "AM" : "PM";
-  document.getElementById("ampm").innerHTML = day;
+  day = hours < 12 ? 'AM' : 'PM';
+  document.getElementById('ampm').innerHTML = day;
   //passtoalarm(hours,mins,day);
 }
 setInterval(clock, 1000);
@@ -32,51 +32,50 @@ setInterval(clock, 1000);
 //////////////////////////////////////////////////////////
 // tab settins
 /////////////////////////////////////////////////////////
-var tabButtons = document.querySelectorAll(".tab .buttonContainer button");
-var tabPanels = document.querySelectorAll(".tab .tabPanel");
+var tabButtons = document.querySelectorAll('.tab .buttonContainer button');
+var tabPanels = document.querySelectorAll('.tab .tabPanel');
 
 function showPanel(panelIndex) {
   tabButtons.forEach(function(node) {
-    node.getElementsByClassName.backgroundColor = "";
-    node.style.color = "";
+    node.getElementsByClassName.backgroundColor = '';
+    node.style.color = '';
   });
-  tabButtons[panelIndex].style.color = "red";
+  tabButtons[panelIndex].style.color = 'red';
   tabPanels.forEach(function(node) {
-    node.style.display = "none";
+    node.style.display = 'none';
   });
-  tabPanels[panelIndex].style.display = "block";
+  tabPanels[panelIndex].style.display = 'block';
 }
 showPanel(0);
 /////////////////////////////////////////////////////////
 // ALarm settings
 ////////////////////////////////////////////////////////
 
-var audio = new Audio("alarmbeeps.mp3");
-document.querySelector("#todo").addEventListener("click", alarm);
+document.querySelector('#todo').addEventListener('click', alarm);
 
 function alarm() {
-  var newhr = parseInt(document.getElementById("add_hour").value);
-  var newmin = parseInt(document.getElementById("add_minute").value);
-  var val = document.getElementById("daynight").value;
-  console.log(hours, mins, secs, day, newhr, newmin, val);
+  const newhr = parseInt(document.getElementById('add_hour').value);
+  const newmin = parseInt(document.getElementById('add_minute').value);
+  const val = document.getElementById('daynight').value;
+  var rooster = new Audio('alarmbeeps.mp3');
 
-  var item = document.getElementById("input").value;
-  var ul = document.getElementById("list");
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode("- " + item));
+  const item = document.getElementById('input').value;
+  let ul = document.getElementById('list');
+  let li = document.createElement('li');
+  li.appendChild(document.createTextNode('- ' + item));
   ul.appendChild(li);
-  document.getElementById("input").value = "";
+  document.getElementById('input').value = '';
 
   var check = setInterval(() => {
     if (hours == newhr && mins == newmin && day == val) {
-      buzz();
       clearInterval(check);
+      buzz();
     }
-  }, 2000);
+  }, 3000);
   function buzz() {
-    audio.play();
+    rooster.play();
     var baz = window.alert(item);
-    audio.pause();
+    rooster.pause();
   }
 }
 
@@ -84,48 +83,47 @@ function alarm() {
 //                          STOPWATCH.EXE
 /////////////////////////////////////////////////////////////////////////////
 
-document.querySelector(".watch").addEventListener("click", watch);
+document.querySelector('.watch').addEventListener('click', watch);
 function watch() {
-  debugger;
-  var gethour = parseInt(document.getElementById("add_hr").value);
-  var getmin = parseInt(document.getElementById("add_min").value);
-  var getsec = parseInt(document.getElementById("add_sec").value);
+  let gethour = parseInt(document.getElementById('add_hr').value);
+  let getmin = parseInt(document.getElementById('add_min').value);
+  let getsec = parseInt(document.getElementById('add_sec').value);
   var startTimer = setInterval(timerset, 1000);
   var startChecking = setInterval(checkvalue, 1000);
-  var rooster = new Audio("alarmbeeps.mp3");
+  var rooster = new Audio('alarmbeeps.mp3');
   var reset = document
-    .querySelector(".reset")
-    .addEventListener("click", resetAlarm);
+    .querySelector('.reset')
+    .addEventListener('click', resetAlarm);
   function timerset() {
     if (getsec > 0) {
       var temp1 = getsec - 1;
       getsec = temp1;
-      document.getElementById("sec").innerHTML = temp1;
-      document.getElementById("hr").innerHTML = gethour + ":";
-      document.getElementById("min").innerHTML = getmin + ":";
+      document.getElementById('sec').innerHTML = temp1;
+      document.getElementById('hr').innerHTML = gethour + ':';
+      document.getElementById('min').innerHTML = getmin + ':';
     } else if (getsec == 0) {
       while (getmin != 0) {
         var temp2 = getmin - 1;
         getmin = temp2;
         getsec = 59;
-        document.getElementById("sec").innerHTML = getsec;
-        document.getElementById("hr").innerHTML = gethour + ":";
-        document.getElementById("min").innerHTML = temp2 + ":";
+        document.getElementById('sec').innerHTML = getsec;
+        document.getElementById('hr').innerHTML = gethour + ':';
+        document.getElementById('min').innerHTML = temp2 + ':';
       }
     } else if (getmin == 0) {
       while (gethour != 0) {
         var temp3 = gethour - 1;
         getmin = temp3;
-        document.getElementById("hr").innerHTML = temp3 + ":";
-        document.getElementById("min").innerHTML = getmin + ":";
-        document.getElementById("sec").innerHTML = getsec;
+        document.getElementById('hr').innerHTML = temp3 + ':';
+        document.getElementById('min').innerHTML = getmin + ':';
+        document.getElementById('sec').innerHTML = getsec;
       }
     }
-    console.log("i am running too");
+    console.log('i am running too');
   }
 
   function checkvalue() {
-    if (getmin == "0" && gethour == "0" && getsec == "0") {
+    if (getmin == '0' && gethour == '0' && getsec == '0') {
       clearInterval(startTimer);
       clearInterval(startChecking);
       playsound();
@@ -136,14 +134,14 @@ function watch() {
 
   function resetAlarm() {
     clearInterval(startTimer);
-    document.getElementById("hr").innerHTML = 0 + ":";
-    document.getElementById("min").innerHTML = 0 + ":";
-    document.getElementById("sec").innerHTML = 0;
+    document.getElementById('hr').innerHTML = 0 + ':';
+    document.getElementById('min').innerHTML = 0 + ':';
+    document.getElementById('sec').innerHTML = 0;
   }
 
   function playsound() {
     rooster.play();
-    var bam = window.alert("Time up");
+    var bam = window.alert('Time up');
     rooster.pause();
   }
 }
